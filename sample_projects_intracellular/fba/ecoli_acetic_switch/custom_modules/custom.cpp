fc1c8c2cc13f2d18b4832c1aefdd2b2009246052
Miguel Ponce-de-Leon <miguel.ponce@bsc.es>
2024-08-29 14:04:14 +0200

    Adding molecular model to Cell class

---
+++ sample_projects_intracellular/fba/ecoli_acetic_switch/custom_modules/custom.cpp
@@ -0,0 +1,306 @@
+/*
+###############################################################################
+# If you use PhysiCell in your project, please cite PhysiCell and the version #
+# number, such as below:                                                      #
+#                                                                             #
+# We implemented and solved the model using PhysiCell (Version x.y.z) [1].    #
+#                                                                             #
+# [1] A Ghaffarizadeh, R Heiland, SH Friedman, SM Mumenthaler, and P Macklin, #
+#     PhysiCell: an Open Source Physics-Based Cell Simulator for Multicellu-  #
+#     lar Systems, PLoS Comput. Biol. 14(2): e1005991, 2018                   #
+#     DOI: 10.1371/journal.pcbi.1005991                                       #
+#                                                                             #
+# See VERSION.txt or call get_PhysiCell_version() to get the current version  #
+#     x.y.z. Call display_citations() to get detailed information on all cite-#
+#     able software used in your PhysiCell application.                       #
+#                                                                             #
+# Because PhysiCell extensively uses BioFVM, we suggest you also cite BioFVM  #
+#     as below:                                                               #
+#                                                                             #
+# We implemented and solved the model using PhysiCell (Version x.y.z) [1],    #
+# with BioFVM [2] to solve the transport equations.                           #
+#                                                                             #
+# [1] A Ghaffarizadeh, R Heiland, SH Friedman, SM Mumenthaler, and P Macklin, #
+#     PhysiCell: an Open Source Physics-Based Cell Simulator for Multicellu-  #
+#     lar Systems, PLoS Comput. Biol. 14(2): e1005991, 2018                   #
+#     DOI: 10.1371/journal.pcbi.1005991                                       #
+#                                                                             #
+# [2] A Ghaffarizadeh, SH Friedman, and P Macklin, BioFVM: an efficient para- #
+#     llelized diffusive transport solver for 3-D biological simulations,     #
+#     Bioinformatics 32(8): 1256-8, 2016. DOI: 10.1093/bioinformatics/btv730  #
+#                                                                             #
+###############################################################################
+#                                                                             #
+# BSD 3-Clause License (see https://opensource.org/licenses/BSD-3-Clause)     #
+#                                                                             #
+# Copyright (c) 2015-2018, Paul Macklin and the PhysiCell Project             #
+# All rights reserved.                                                        #
+#                                                                             #
+# Redistribution and use in source and binary forms, with or without          #
+# modification, are permitted provided that the following conditions are met: #
+#                                                                             #
+# 1. Redistributions of source code must retain the above copyright notice,   #
+# this list of conditions and the following disclaimer.                       #
+#                                                                             #
+# 2. Redistributions in binary form must reproduce the above copyright        #
+# notice, this list of conditions and the following disclaimer in the         #
+# documentation and/or other materials provided with the distribution.        #
+#                                                                             #
+# 3. Neither the name of the copyright holder nor the names of its            #
+# contributors may be used to endorse or promote products derived from this   #
+# software without specific prior written permission.                         #
+#                                                                             #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" #
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   #
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  #
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   #
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         #
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        #
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    #
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     #
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     #
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  #
+# POSSIBILITY OF SUCH DAMAGE.                                                 #
+#                                                                             #
+###############################################################################
+*/
+
+#include "custom.h"
+
+
+void create_cell_types(void)
+{
+	SeedRandom(parameters.ints("random_seed"));
+
+	initialize_default_cell_definition();
+
+	/*  This parses the cell definitions in the XML config file.  */
+	initialize_cell_definitions_from_pugixml();
+
+	//  This sets the pre and post intracellular update functions
+	cell_defaults.functions.pre_update_intracellular =  pre_update_intracellular;
+	cell_defaults.functions.post_update_intracellular = post_update_intracellular;
+	cell_defaults.functions.update_phenotype = NULL; 
+	
+
+	build_cell_definitions_maps();
+	
+	setup_signal_behavior_dictionaries();
+
+	display_cell_definitions(std::cout);
+
+	return;
+}
+
+
+
+
+void setup_microenvironment(void)
+{
+	initialize_microenvironment();
+	return;
+}
+
+void setup_tissue(void)
+{
+
+	double cell_radius = cell_defaults.phenotype.geometry.radius; 
+	double colony_radius =  parameters.doubles("colony_radius");
+
+	std::vector<std::vector<double>> positions;
+	if (default_microenvironment_options.simulate_2D == true)
+		positions = create_cell_disc_positions(cell_radius,colony_radius); 
+	else
+		positions = create_cell_sphere_positions(cell_radius,colony_radius);
+
+	Cell* pCell = NULL; 
+	for (int i = 0; i < positions.size(); i++)
+	{
+		pCell = create_cell(get_cell_definition("default"));
+		pCell->assign_position(positions[i]);
+	}	
+	return; 
+}
+
+void pre_update_intracellular(PhysiCell::Cell* pCell, PhysiCell::Phenotype& phenotype, double dt ){
+	PhysiCelldFBA::update_dfba_inputs( pCell, phenotype, dt );
+	PhysiCelldFBA::
+	return;
+}
+
+
+void post_update_intracellular(PhysiCell::Cell* pCell, PhysiCell::Phenotype& phenotype, double dt ){
+	PhysiCelldFBA::update_dfba_outputs( pCell, phenotype, dt );
+	return;
+}
+
+
+
+
+void update_cell(PhysiCell::Cell* pCell, PhysiCell::Phenotype& phenotype, double dt ){
+
+	dFBAIntracellular *model = (dFBAIntracellular*) phenotype.intracellular;
+	model->update(pCell, phenotype, dt);
+	
+	if (phenotype.volume.total	>= 5000 ){
+		pCell->divide();
+	}
+  
+}
+
+
+void metabolic_cell_phenotype( Cell* pCell, Phenotype& phenotype, double dt )
+{
+	// if cell is dead, don't bother with future phenotype changes.
+	if( phenotype.death.dead == true )
+	{
+		pCell->functions.update_phenotype = NULL;
+		return;
+	}
+
+	// update the transition rate according to growth rate?
+	static int cycle_start_index = live.find_phase_index( PhysiCell_constants::live );
+	static int cycle_end_index = live.find_phase_index( PhysiCell_constants::live );
+
+	//static int oncoprotein_i = pCell->custom_data.find_variable_index( "oncoprotein" );
+	//phenotype.cycle.data.transition_rate( cycle_start_index ,cycle_end_index ) *= pCell->custom_data[oncoprotein_i] ;
+	return;
+}
+
+
+std::vector<std::vector<double>> create_cell_sphere_positions(double cell_radius, double sphere_radius)
+{
+	std::vector<std::vector<double>> cells;
+	int xc=0,yc=0,zc=0;
+	double x_spacing= cell_radius*sqrt(3);
+	double y_spacing= cell_radius*2;
+	double z_spacing= cell_radius*sqrt(3);
+	
+	std::vector<double> tempPoint(3,0.0);
+	// std::vector<double> cylinder_center(3,0.0);
+	
+	for(double z=-sphere_radius;z<sphere_radius;z+=z_spacing, zc++)
+	{
+		for(double x=-sphere_radius;x<sphere_radius;x+=x_spacing, xc++)
+		{
+			for(double y=-sphere_radius;y<sphere_radius;y+=y_spacing, yc++)
+			{
+				tempPoint[0]=x + (zc%2) * 0.5 * cell_radius;
+				tempPoint[1]=y + (xc%2) * cell_radius;
+				tempPoint[2]=z;
+				
+				if(sqrt(norm_squared(tempPoint))< sphere_radius)
+				{ cells.push_back(tempPoint); }
+			}
+			
+		}
+	}
+	return cells;
+	
+}
+
+
+std::vector<std::vector<double>> create_cell_disc_positions(double cell_radius, double disc_radius)
+{	 
+	double cell_spacing = 0.95 * 2.0 * cell_radius; 
+	
+	double x = 0.0; 
+	double y = 0.0; 
+	double x_outer = 0.0;
+
+	std::vector<std::vector<double>> positions;
+	std::vector<double> tempPoint(3,0.0);
+	
+	int n = 0; 
+	while( y < disc_radius )
+	{
+		x = 0.0; 
+		if( n % 2 == 1 )
+		{ x = 0.5 * cell_spacing; }
+		x_outer = sqrt( disc_radius*disc_radius - y*y ); 
+		
+		while( x < x_outer )
+		{
+			tempPoint[0]= x; tempPoint[1]= y;	tempPoint[2]= 0.0;
+			positions.push_back(tempPoint);			
+			if( fabs( y ) > 0.01 )
+			{
+				tempPoint[0]= x; tempPoint[1]= -y;	tempPoint[2]= 0.0;
+				positions.push_back(tempPoint);
+			}
+			if( fabs( x ) > 0.01 )
+			{ 
+				tempPoint[0]= -x; tempPoint[1]= y;	tempPoint[2]= 0.0;
+				positions.push_back(tempPoint);
+				if( fabs( y ) > 0.01 )
+				{
+					tempPoint[0]= -x; tempPoint[1]= -y;	tempPoint[2]= 0.0;
+					positions.push_back(tempPoint);
+				}
+			}
+			x += cell_spacing; 
+		}		
+		y += cell_spacing * sqrt(3.0)/2.0; 
+		n++; 
+	}
+	return positions;
+}
+
+
+std::vector<std::string> my_coloring_function( Cell* pCell )
+{
+
+	std::vector<std::string> output = false_cell_coloring_cytometry(pCell);
+	output[0] = "red";
+	output[1] = "red";
+	output[2] = "red";
+
+	if( pCell->phenotype.death.dead == false && pCell->type == 1 )
+	{
+		 output[0] = "black";
+		 output[2] = "black";
+	}
+
+	return output;
+}
+
+
+std::vector<std::string> metabolic_coloring_function( Cell* pCell )
+{
+	std::vector< std::string > output( 4, "black" ); 
+	
+	// if( pCell->type == 1 )
+	// { return output; } 
+	
+	// // live cells are green, but shaded by oncoprotein value 
+	// if( pCell->phenotype.death.dead == false )
+	// {
+	// 	int respiration = (int) round( (1.0/(o2_max-o2_min)) * (pCell->custom_data[oncoprotein_i]-p_min) * 255.0 ); 
+	// 	char szTempString [128];
+	// 	sprintf( szTempString , "rgb(%u,%u,%u)", oncoprotein, oncoprotein, 255-oncoprotein );
+	// 	output[0].assign( szTempString );
+	// 	output[1].assign( szTempString );
+
+	// 	sprintf( szTempString , "rgb(%u,%u,%u)", (int)round(output[0][0]/p_max) , (int)round(output[0][1]/p_max) , (int)round(output[0][2]/p_max) );
+	// 	output[2].assign( szTempString );
+		
+	// 	return output; 
+	// }
+
+	/*
+    if(value > 0.5){
+        value -= 0.5;
+        rgb[0] = 0;
+        rgb[1] = (int)((1-2*value)*255);
+        rgb[2] = (int)(2*value*255);
+    }
+    if(value <= 0.5){
+        rgb[0] = (int)((1-2*value)*255);
+        rgb[1] = (int)(2*value*255);
+        rgb[2] = 0;
+    }*/
+	
+	
+	
+	return output; 
+}
